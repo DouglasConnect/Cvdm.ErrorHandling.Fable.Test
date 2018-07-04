6435312642a6d3f5465d5d539fea4b408c98e207
Daniel Bachler <daniel@danielbachler.de>
2018-07-04 13:03:33 +0200

    Initial version - try to use forked Cvdm.Errorhandling with f sharp code included, does not work

---
+++ src/App.fs
@@ -0,0 +1,26 @@
+module Cvdm.consume.test
+
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.Import
+open Cvdm.ErrorHandling
+
+let somefn() =
+    result {
+        return "hello"
+    }
+let init() =
+    let canvas = Browser.document.getElementsByTagName_canvas().[0]
+    canvas.width <- 1000.
+    canvas.height <- 800.
+    let ctx = canvas.getContext_2d()
+    // The (!^) operator checks and casts a value to an Erased Union type
+    // See http://fable.io/docs/interacting.html#Erase-attribute
+    ctx.fillStyle <- !^"rgb(200,0,0)"
+    ctx.fillRect (10., 10., 55., 50.)
+    ctx.fillStyle <- !^"rgba(0, 0, 200, 0.5)"
+    ctx.fillRect (30., 30., 55., 50.)
+    match somefn() with
+    | Ok _ -> ctx.fillRect (100., 100., 55., 50.)
+
+init()
